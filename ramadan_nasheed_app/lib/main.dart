9cda4a8743ba3e530e6a4b67615f85523e7d2114
sumeyahassen <sumeyahassensumi@gmail.com>
2025-12-26 01:58:26 +0300

    perfactly work to inssert 10 neshida colection

--- ramadan_nasheed_app/lib/main.dart
+++ ramadan_nasheed_app/lib/main.dart
@@ -1,13 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:just_audio_media_kit/just_audio_media_kit.dart'; // Required for Linux support
+
 import 'screens/home_screen.dart';
 import 'models/nasheed_model.dart';
 
 void main() {
+  // Initialize the media_kit backend for just_audio on Linux (and other desktop platforms)
+  // This fixes the MissingPluginException you were seeing on Linux
+  JustAudioMediaKit.ensureInitialized(linux: true);
+
   runApp(
     MultiProvider(
       providers: [
-        Provider<List<Nasheed>>(create: (_) => []),
+        Provider<List<Nasheed>>(
+          create: (_) => [], // Initial empty list
+        ),
       ],
       child: const MyApp(),
     ),
@@ -25,7 +33,8 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.green,
         scaffoldBackgroundColor: Colors.transparent,
-        fontFamily: 'Roboto',
+        fontFamily: 'Roboto', // You can change this to any Google Font if you add it
+        visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
       home: const HomeScreen(),
     );
